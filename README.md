ba7525a69960733d891ab0855f81b66732f7ab85
mcmarius <23401453+mcmarius@users.noreply.github.com>
2026-02-25 12:30:52 +0200

    Adaugă titlu și bormașini

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Nu primesc notă pentru că nu am pus titlu și descriere
+# OOP face brr
 
 ### Folosiți template-ul corespunzător grupei voastre!
 
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 // #include "Example.h"
 
 int main() {
-    std::cout << "Hello, 1312!\n";
+    std::cout << "Hello, 1312 cu bormasini!\n";
     Example e1;
     e1.g();
     std::array<int, 100> v{};
